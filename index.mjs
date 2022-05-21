7196098cf9189068888d7fc5844fdc995ee781f1
Jack-Zhang-1314 <zyj17715640603@gmail.com>
2022-05-21 13:22:48 +0800

    add action

---
+++ index.mjs
@@ -0,0 +1,21 @@
+import { Octokit } from "octokit"
+import { getInput } from "@actions/core"
+import dayjs from "dayjs";
+const token = getInput("token")
+const octokit = new Octokit({
+  auth: token
+});
+await octokit.rest.issues.create({
+  owner: "Jack-Zhang-1314",
+  repo: "demo",
+  title: getTitle(),
+  body: getBody()
+});
+
+function getTitle() {
+  return dayjs().format("YYYY-MM-DD")
+}
+
+function getBody() {
+  return "* test a new task"
+}
