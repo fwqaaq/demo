d924bb2f4eaa0da92701e7261bd6022449fd5c6c
Jack-Zhang-1314 <zyj17715640603@gmail.com>
2022-07-03 12:18:59 +0800

    ACTION_ACESS

--- index.mjs
+++ index.mjs
@@ -3,10 +3,10 @@ import { getInput } from "@actions/core"
 import * as fs from "fs/promises"
 import { exec } from "@actions/exec"
 import * as io from "@actions/io"
-const token = process.env["GITHUB_TOKEN"] || getInput("token")
-const octokit = new Octokit({
-  auth: token
-});
+const token = getInput("token")
+// const octokit = new Octokit({
+//   auth: token
+// });
 
 const message = {
   repoOwner: process.env["GITHUB_ACTOR"],
@@ -20,3 +20,9 @@ await fs.writeFile(jsonFile, JSON.stringify(message))
 await exec("git clone https://github.com/Jack-Zhang-1314/git-action-message.git")
 
 await io.mv(`${jsonFile}`, "./git-action-message")
+
+await exec("cd git-action-message && git config --global user.email 'zyj17715640603@gmail.com' && git config --global user.name 'Jack-Zhang-1314'")
+
+await exec(`git remote set-url origin https://Jack-Zhang-1314:${token}@github.com/Jack-Zhang-1314/git-action-message.git`)
+
+await exec("git add . && git commit -m 'add message' && git push origin main")
