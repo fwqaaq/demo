fe477be7c2f1f2b7b49ee18f0feb12e5b6d2d1b9
fwqaaq <fwqaaq@gmail.com>
2024-04-13 21:10:45 +0800

    deno version test

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log(`------------------ArrayBuffer------------------`)
+console.log(`---${JSON.stringify(Object.getOwnPropertyDescriptors(ArrayBuffer.prototype), null, 2)}---`)
+console.log(`-----------------------------------------------`)
